c97ea0d8a9f8168e8887e5472010d4237f3eabc8
bjbfr <benjamin.bouchard.fr@gmail.com>
2023-10-01 09:40:32 +0200

    init fs

---
+++ fs/code/fs.ts
@@ -0,0 +1,205 @@
+namespace Fs {
+
+  export const DIR_SEP = '/';
+  export const DRIVE_ROOT_NAME = DriveApp.getRootFolder().getName();
+
+  export const DOCUMENT_TYPE = 'application/vnd.google-apps.document';
+  export const SPREADSHEET_TYPE = 'application/vnd.google-apps.spreadsheet';
+
+  export enum MineType {
+    DOC = 'application/vnd.google-apps.document',
+    SHEET = 'application/vnd.google-apps.spreadsheet'
+  }
+
+  // export type FileType = keyof typeof FileTypeEnum
+  // export type FileDesc = {id: string, name: string, type: GoogleAppsScript.Base.MimeType}
+  export type FileDesc = { id: string, name: string, type: MineType | undefined }
+
+  export function openById({id,type}:FileDesc){
+    if (type === MineType.SHEET)
+      return SpreadsheetApp.openById(id);
+    else if (type === MineType.DOC)
+      return DocumentApp.openById(id)
+    return;
+  }
+
+  /**
+   * 
+   * @param srcDir 
+   * @param srcFile 
+   * @param dstDir 
+   * @param dstFile 
+   * @param appName 
+   * @returns 
+   */
+  export function copy_file(srcDir: AbsolutePath, srcFile: string, dstDir: AbsolutePath, dstFile: string) {
+
+    // get srcFile id (and check if it exists) 
+    let srcFileIter = DriveApp.getFilesByName(srcFile);
+    const srcFileId = get_elt_id(srcDir, srcFileIter);
+    if (!srcFileId) {
+      const message = `Cannot find file ${srcFile} in directory: ${srcDir}.`;
+      Logger.log(message);
+      Ui.alert_user("Warning", message);
+      return;
+    }
+    // find/create dstDir
+    const dstDirId = find_mk_dir(dstDir);
+    // list dir content
+    const ls_dirs = list_dir(dstDirId);
+    // check if target already exists in dstDir, if yes move it to trash
+    const ls_dir = ls_dirs.find(({ name }) => name === dstFile);
+    if (ls_dir) {
+      const { id } = ls_dir
+      DriveApp.getFileById(id).setTrashed(true);
+    }
+    //copy 'srcFile' into 'dstDir' with name 'dstFile'
+    return DriveApp.getFileById(srcFileId).makeCopy(dstFile, DriveApp.getFolderById(dstDirId));
+  }
+  /**
+   * Lists content of directory represented by its {@link dirId}
+   * @param dirId {string}
+   * @returns 
+   */
+  export function list_dir(dirId: string): Array<FileDesc> {
+    let ret = [];
+    let iter = DriveApp.getFolderById(dirId).getFiles();
+    while (iter.hasNext()) {
+      let file = iter.next();
+      ret.push({ id: file.getId(), name: file.getName(), type: Utils.fromString(MineType, file.getMimeType()) });
+    }
+    return ret;
+  }
+  /**
+   * Finds a directory from its path {@link pathDir} and returns its id.
+   * Creates all missing elements along the directories chain.
+   * @param pathDir {AbsolutePath}
+   * @returns 
+   */
+  export function find_mk_dir(pathDir: Path): string {
+
+    // dstDirIds = [{root,dirName,id}] for directories along the dstDir 
+    const dstDirIds = path_parts(pathDir)
+      .reduce(({ acc, root }, x) => {
+        acc.push([root, x]);
+        return { acc: acc, root: `${root}${x}${DIR_SEP}` };
+      }, { acc: [], root: DIR_SEP } as { acc: Array<[AbsolutePath, string]>, root: string }
+      )["acc"]
+      .map(
+        ([root, dirName]) => {
+          return { root: root, dirName: dirName, id: get_elt_id(root, DriveApp.getFoldersByName(dirName)) }
+        }
+      );
+
+    if (dstDirIds.length === 0)
+      return DriveApp.getRootFolder().getId();
+
+    // Logger.log(`dstDirIds: ${JSON.stringify(dstDirIds)}`);
+
+    // detect the first non-existing directory (if any)
+    let index = dstDirIds.findIndex(({ id }) => id === undefined)
+    if (index !== -1) {
+      let prev_dir: GoogleAppsScript.Drive.Folder;
+      if (index === 0) {
+        prev_dir = DriveApp.getRootFolder();
+      } else {
+        let prev_id = dstDirIds[index - 1].id as string; // cannot be undefined
+        prev_dir = DriveApp.getFolderById(prev_id);
+      }
+      //create folders
+      dstDirIds.slice(index).forEach(({ dirName }) => prev_dir = prev_dir.createFolder(dirName));
+      return prev_dir.getId();
+    } else {
+      return dstDirIds[dstDirIds.length - 1].id as string; // cannot be undefined
+    }
+  }
+
+  /**
+   * Retrieves the id of an element (file or folder) based on its name and an iterator of possible candidates.
+   * Returns undefined if none of the candidates matches the name.
+   * @param root {AbsolutePath}
+   * @param iter {GoogleAppsScript.Drive.FileIterator|GoogleAppsScript.Drive.FolderIterator}
+   * @returns 
+   */
+  export function get_elt_id(root: AbsolutePath, iter: GoogleAppsScript.Drive.FileIterator | GoogleAppsScript.Drive.FolderIterator): string | undefined {
+
+    while (iter.hasNext()) {
+      let elt = iter.next();
+      let path = get_elt_path(elt);
+      if (path === root) {
+        return elt.getId();
+      }
+    }
+    return;
+  }
+
+  /**
+   * Returns the path of {@link elt}
+   * @param elt {GoogleAppsScript.Drive.File|GoogleAppsScript.Drive.Folder} 
+   * @returns 
+   */
+  export function get_elt_path(elt: GoogleAppsScript.Drive.File | GoogleAppsScript.Drive.Folder): string | undefined {
+
+    if (!elt) return;
+
+    let path = [];
+
+    let iter = elt.getParents();
+    while (iter.hasNext()) {
+      let folder = iter.next();
+      path.push(folder.getName());
+      iter = folder.getParents();
+    }
+
+    return drive_to_unix(
+      path_from_parts(path.reverse())
+    );
+
+  }
+
+  /**
+   * Splits a path into parts (reverse operation of path_from_parts)
+   * @param path 
+   * @returns 
+   */
+  export function path_parts(path: Path): Array<string> {
+    return path.split(DIR_SEP).filter(p => p !== '');
+  }
+
+  /**
+   * Assembles {@link parts} into a path (reverse operation of path_parts)
+   * @param parts 
+   * @returns 
+   */
+  export function path_from_parts(parts: Array<string>): AbsolutePath {
+    if (parts.length == 0) {
+      return DIR_SEP;
+    } else {
+      return DIR_SEP + parts.join(DIR_SEP) + DIR_SEP;
+    }
+  }
+
+  /**
+   * Converts a unix path into a drive one (reverse operation of drive_to_unix)
+   * @param path {AbsolutePath}
+   * @returns 
+   */
+  export function unix_to_drive(path: AbsolutePath): AbsolutePath {
+    if (!path.startsWith(`${DIR_SEP}${DRIVE_ROOT_NAME}`))
+      return `${DIR_SEP}${DRIVE_ROOT_NAME}${path}`;
+    else
+      return path;
+  }
+
+  /**
+   * Converts a drive path into a unix one (reverse operation of unix_to_drive)
+   * @param path {AbsolutePath}
+   * @returns 
+   */
+  export function drive_to_unix(path: AbsolutePath): AbsolutePath {
+    return path_from_parts(
+      path_parts(path).filter(p => p != DRIVE_ROOT_NAME)
+    )
+  }
+
+}
---
+++ fs/code/tests/fs.test.ts
@@ -0,0 +1,113 @@
+/// <reference path="../fs.ts"/>
+
+//test_get_file_path
+function fs_test_get_file_path() {
+  return Unittest.run<GoogleAppsScript.Drive.File | GoogleAppsScript.Drive.Folder, string | undefined, string>(
+    "test_get_file_path",
+    [
+      { input: DriveApp.getFolderById('17zu3McC6cCKpsbjwG9YfaMixA_48jNp2'), expected: "/SASU-BB-IMMO/src/" }, // directory TEMPLATES in /SASU-BB-IMMO/src/ (/SASU-BB-IMMO/src/TEMPLATES) 
+      { input: DriveApp.getFileById('1tmgZe4Yd7wf26CbXZrpX20VdYoXqxny8'), expected: "/FormationML/" }  // file "product_category_name_translation.csv" in /FormationML/
+    ],
+    (input, _) => Fs.get_elt_path(input),
+    (input) => input.getId()
+  );
+}
+//test_path_parts
+function fs_test_path_parts() {
+  return Unittest.run<string, Array<string>>(
+    "path parts",
+    [{ input: `/${Fs.DRIVE_ROOT_NAME}/a/b/c/`, expected: [Fs.DRIVE_ROOT_NAME, 'a', 'b', 'c'] }],
+    (input, _) => Fs.path_parts(input)
+  );
+}
+//test_unix_to_drive
+function fs_test_unix_to_drive() {
+  return Unittest.run<string, string>(
+    "unix to drive",
+    [
+      { input: "/a/b/c/", expected: `/${Fs.DRIVE_ROOT_NAME}/a/b/c/` },
+      { input: `/${Fs.DRIVE_ROOT_NAME}/a/b/c/`, expected: `/${Fs.DRIVE_ROOT_NAME}/a/b/c/` }
+    ],
+    (input, _) => Fs.unix_to_drive(input)
+  );
+}
+//test_drive_to_unix
+function fs_test_drive_to_unix() {
+  return Unittest.run<string, string>(
+    "drive to unix",
+    [
+      { input: `/${Fs.DRIVE_ROOT_NAME}/a/b/c/`, expected: "/a/b/c/" },
+      { input: "/a/b/c/", expected: "/a/b/c/" }
+    ],
+    (input, _) => Fs.drive_to_unix(input)
+  );
+}
+//test_unix_to_drive_to_unix
+function fs_test_unix_to_drive_to_unix() {
+  return Unittest.run<string, string>(
+    "unix to drive to unix",
+    [
+      { input: `/${Fs.DRIVE_ROOT_NAME}/a/b/c/`, expected: '/a/b/c/' },
+      { input: '/a/b/c/', expected: '/a/b/c/' }
+    ],
+    (input, _) => Fs.drive_to_unix(Fs.unix_to_drive(input))
+  );
+}
+//test_drive_to_unix_to_drive
+function fs_test_drive_to_unix_to_drive() {
+  return Unittest.run<string, string>(
+    "drive to unix to drive",
+    [
+      { input: `/${Fs.DRIVE_ROOT_NAME}/a/b/c/`, expected: `/${Fs.DRIVE_ROOT_NAME}/a/b/c/` },
+      { input: '/a/b/c/', expected: `/${Fs.DRIVE_ROOT_NAME}/a/b/c/` }
+    ],
+    (input, _) => Fs.unix_to_drive(Fs.drive_to_unix(input))
+  );
+}
+//test_list_dir
+function fs_test_list_dir() {
+
+  return Unittest.run<string, Array<Fs.FileDesc>>(
+    "list dir",
+    [
+      {
+        input: '13sGA0_gXxq0OVwRePOHfNXV3Tns-iE_a', 
+        expected: [
+          {id:"11RqCdU_z6D0Anv-u838UtEamENVhmfU1ZiBAQKeMv9k",name:"list_sheet",type:Fs.MineType.SHEET},
+          {id:"1XBEf0niRzIh1vAuXov7RoAt3lvnpP27cSRF4Qplm3RM",name:"list_doc",type:Fs.MineType.DOC}
+        ]
+      }
+    ],
+    (input, _) => Fs.list_dir(input)
+  )
+
+}
+
+function fs_test_all(){
+  return Unittest.run_all([
+    "fs_test_get_file_path",
+    "fs_test_path_parts",
+    "fs_test_unix_to_drive",
+    "fs_test_drive_to_unix",
+    "fs_test_unix_to_drive_to_unix",
+    "fs_test_drive_to_unix_to_drive",
+    "fs_test_list_dir"
+  ])
+}
+
+
+// type MineType = keyof GoogleAppsScript.Base.MimeType
+
+// function fs_test_mine_type(){
+//   return [
+//     {id:"11RqCdU_z6D0Anv-u838UtEamENVhmfU1ZiBAQKeMv9k",name:"list_sheet",type:"GOOGLE_DOCS"},
+//     {id:"1XBEf0niRzIh1vAuXov7RoAt3lvnpP27cSRF4Qplm3RM",name:"list_doc",type:Fs.DOCUMENT_TYPE}
+//   ].map(
+//     x => ({name:x.name,type:x.type,mine_type: DriveApp.getFileById(x.id).getMimeType()})
+//   )
+// }
+
+// function test_copy_file(){
+//   Fs.copy_file("/SASU-BB-IMMO/","bb-immo-7021-iban-fr.pdf","/SASU-BB-IMMO/test/a/b/c","");
+//   Fs.copy_file("/SASU-BB-IMMO/","bb-immo-7021-iban-fr.pdf","/riri/fifi/loulou","");
+// }
